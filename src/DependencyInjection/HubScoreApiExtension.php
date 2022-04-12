ce69ca02ad21c1f345bcd9d907c4c6bae5acad0a
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2022-04-12 17:13:35 +0200

    Update HubScoreApiExtension.php

--- src/DependencyInjection/HubScoreApiExtension.php
+++ src/DependencyInjection/HubScoreApiExtension.php
@@ -4,21 +4,21 @@ namespace SevenGroupFrance\HubScoreApiBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
-class HubScoreApiExtension extends ConfigurableExtension
+class HubScoreApiExtension extends Extension
 {
-    public function loadInternal(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-        
-        $definition = $container->getDefinition('twitter');
-        $definition->replaceArgument(0, $config['client_id']);
-        $definition->replaceArgument(1, $config['client_secret']);
+
+        $definition = $container->getDefinition('seven_group_france.hub_score_api.api_call');
+        $definition->replaceArgument(0, $config['login']['id']);
+        $definition->replaceArgument(1, $config['login']['pswd']);
     }
 }
