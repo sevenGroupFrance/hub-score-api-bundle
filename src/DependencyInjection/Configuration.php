660715a4514674d1f8949b2634ca2d80006cefa2
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2022-04-12 11:21:25 +0200

    Add files via upload

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,28 @@
+<?php
+
+// src/Acme/SocialBundle/DependencyInjection/Configuration.php
+namespace SevenGroupFrance\HubScoreApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('hub_score_api');
+
+        $treeBuilder->getRootNode()
+            ->children()
+                ->arrayNode('twitter')
+                    ->children()
+                        ->integerNode('client_id')->end()
+                        ->scalarNode('client_secret')->end()
+                    ->end()
+                ->end() // twitter
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
