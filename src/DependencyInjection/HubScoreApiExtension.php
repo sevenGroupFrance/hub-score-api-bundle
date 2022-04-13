d76aa3a849c295c0e4e52e8837d6e67b518a9e61
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2022-04-13 16:56:57 +0200

    Update HubScoreApiExtension.php

--- src/DependencyInjection/HubScoreApiExtension.php
+++ src/DependencyInjection/HubScoreApiExtension.php
@@ -11,14 +11,14 @@ class HubScoreApiExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
         $definition = $container->getDefinition('seven_group_france.hub_score_api.event_subscriber.hub_score_api');
         $definition->replaceArgument(0, $config['login']['id']);
         $definition->replaceArgument(1, $config['login']['pswd']);
-
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.xml');
     }
 }
